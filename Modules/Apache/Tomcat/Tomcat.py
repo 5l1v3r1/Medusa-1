e6be266834b4f2d1be21420977582d1e09d4f6a0
橙子 <33721903+Ascotbe@users.noreply.github.com>
2020-05-04 16:16:45 +0800

    Revert "pull master"

--- Modules/Apache/Tomcat/Tomcat.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# _*_ coding: utf-8 _*_
-from Modules.Apache.Tomcat import TomcatUnauthorizedCommandExecutionVulnerability
-from ClassCongregation import Prompt
-
-def Main(ThreadPool,Url,Values,Token,Proxies):
-    ThreadPool.Append(TomcatUnauthorizedCommandExecutionVulnerability.medusa, Url, Values, Token,proxies=Proxies)
-    Prompt("Tomcat")
-
--- Modules/Cms/Pboot/Pboot.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# _*_ coding: utf-8 _*_
-from Modules.Cms.Pboot import PbootCommandExecution
-from Modules.Cms.Pboot import PbootOnlineMessageDeskSqlInjection
-from ClassCongregation import Prompt
-def Main(ThreadPool,Url,Values,Token,proxies):
-    ThreadPool.Append(PbootCommandExecution.medusa, Url, Values, Token,proxies=proxies)
-    ThreadPool.Append(PbootOnlineMessageDeskSqlInjection.medusa, Url, Values, Token,proxies=proxies)
-    Prompt("Pboot")
